007c5f94346771f745fd003315666a20d2ae25bd
gabrielpastega <gabrielpbreis@gmail.com>
2024-11-06 23:12:33 -0300

    refactor: rewrote the code to use crawlers

--- src/request/ml_search.py
+++ src/request/ml_search.py
@@ -1,35 +1,52 @@
 import requests
+from bs4 import BeautifulSoup
 import pandas as pd
 
-from bs4 import BeautifulSoup
+class MercadoLivreCrawler:
+    def execute_command(self, query):
+        url = f"https://lista.mercadolivre.com.br/{query.replace(' ', '-')}"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+        }
+
+        proxy = {
+            'http': 'http://localhost:8080',
+            'https': 'http://localhost:8080',
+        }
 
 
-keyword = 'Sabonete'
+        response = requests.get(url, headers=headers, proxies=proxy, verify=False)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            results = soup.find_all("div", class_="ui-search-result")
 
-url = f"https://lista.mercadolivre.com.br/{keyword}"
+            data = []
+            for result in results:
+                link = None
+                title = result.find("h2", class_="ui-search-item__title").text.strip()
+                price = result.find("span", class_="andes-money-amount__fraction").text.strip()
+                link_tag =  result.find("a", class_="ui-search-link")
+                if link_tag:
+                    link = link_tag.get("href")
+                data.append({"Produto": title, "Preço": price, "URL": link})
 
-response = requests.get(url)
+            return data
+        else:
+            print("Falha ao fazer a solicitação HTTP.")
+            return None
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
+    def send_dataframe(self, query):
+        data = self.execute_command(query)
+        if data:
+            df = pd.DataFrame(data)
+            return df
+        else:
+            return None
 
-    search_result = soup.find_all("ol", class_="ui-search-layout")
-
-    data = []
-
-    for result in search_result:
-        title = result.find("h2", class_="poly-component__title").text.strip()
-        link = result.find("a", class_="href")
-        price = result.find("span", class_="poly-price__current").text.strip()
-        brand = result.find("span", class_="poly-component__brand").text.strip()
-
-        if link:
-            link = link.get("href")
-
-        data.append({"Title": title, "Brand": brand, "Price":price, "Link":link})
-        
-    
-    print(data)
-
+# Exemplo de utilização
+crawler = MercadoLivreCrawler()
+dataframe = crawler.send_dataframe("iphone 12")
+if dataframe is not None:
+    print(dataframe)
 else:
-    print("Error")
+    print("Erro ao obter os dados.")
